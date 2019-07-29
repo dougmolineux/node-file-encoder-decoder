fbd489ca2e41f016dafd5e5d5a071eafd0b2c1f1
Doug Molineux <doug.molineux@gmail.com>
2019-07-29 15:00:01 -0600

    initial commit

---
+++ app.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+
+const filepath = process.argv[2];
+const newFilename = filepath.split('.')[0] + "_copy." + filepath.split('.')[1];
+const blobFileName = 'blob';
+
+console.log(`Reading ${filepath}...`);
+const originalBitmap = fs.readFileSync(filepath, { encoding: 'base64' });
+
+console.log(`Writing file to ${blobFileName}...`);
+fs.writeFileSync(blobFileName, originalBitmap);
+
+console.log(`Reading ${blobFileName}...`);
+const blobBitmap = fs.readFileSync(blobFileName).toString('binary');
+
+console.log(`Copying file to ${newFilename}...`);
+// fs.writeFileSync(newFilename, blobBitmap, { encoding: 'base64' });
+
+fs.writeFile(newFilename, blobBitmap, 'binary', function(err) {
+  console.log('File created');
+});
